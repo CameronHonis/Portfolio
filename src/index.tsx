33f6d650ab3e68ee5760571aec05c0320acb0dfc
Cameron <cameron-04@hotmail.com>
2024-04-29 10:07:23 -0500

    add posthog analytics

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { App } from './App';
+import {App} from './App';
 
+import posthog from "posthog-js";
+import {PostHogEvent} from "./models/PostHogEvents";
+
+posthog.init('phc_VbKnqLMfLYOMETXlZ0gJxCp9ubs9jWfuawo33PZLLCJ', {api_host: 'https://us.i.posthog.com'})
+
+posthog.capture(PostHogEvent.PAGE_INIT);
 ReactDOM.render(
-    <App />,
-  document.getElementById('root')
+    <App/>,
+    document.getElementById('root')
 );
---
+++ src/models/PostHogEvents.ts
@@ -0,0 +1,3 @@
+export enum PostHogEvent {
+    PAGE_INIT = 'page_init',
+}
